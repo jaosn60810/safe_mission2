f5e8e0560331c3497bcaa1f37ff627f3e6a22c4e
jason60810 <jason60810@gmail.com>
2022-05-14 23:50:23 +0800

    feat: 完成 "彈性選擇" 手機版 和 js

--- src/app.js
+++ src/app.js
@@ -11,3 +11,34 @@ const carouselMobile = new bootstrap.Carousel(myCarousel, {
   interval: false,
   wrap: false,
 });
+
+const programBtnGroup = document.querySelector('.programBtns');
+
+programBtnGroup.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'A') {
+    return;
+  }
+
+  // 切換按下按鈕樣式
+  const programBtns = programBtnGroup.querySelectorAll('.btn');
+  programBtns.forEach((item) => {
+    item.classList.remove('active');
+  });
+  e.target.classList.toggle('active');
+
+  // 更換文字
+  const newNumLen = e.target.textContent.length;
+  const newNum = e.target.textContent.slice(0, newNumLen - 1);
+  const nums = document.querySelectorAll('.num');
+  nums.forEach((item) => {
+    // 如果是 >25000 要把字變小，避免跑版
+    if (newNum === '>25000') {
+      item.parentElement.classList.remove('fs-3');
+      item.parentElement.classList.add('fs-4');
+    }
+
+    item.textContent = newNum;
+  });
+});
